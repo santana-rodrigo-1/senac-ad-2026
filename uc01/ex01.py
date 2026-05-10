aa55f06325e22dfff5e46a9835e28437aec263a4
santana-rodrigo-1 <santana-rodrigo@aluno.senac.rio>
2026-05-10 17:35:19 -0300

    senac-ad-2026

--- uc01/ex01.py
+++ uc01/ex01.py
@@ -3,3 +3,7 @@
 #1) print("Olá, mundo!!! ")
 #
 print("Olá, mundo!!!")
+
+
+# python uc01/x01.py # executar o arquivo
+
--- uc01/ex07.py
+++ uc01/ex07.py
@@ -9,7 +9,7 @@ n2=float(input("Digite a nota 2 = "))
 print(n2)
 m = (n1+n2)/2
 print (f"A média entre {n1} e {n2} é: {m}")
-print (f"Nota {n1} + {n2} = {m})
+print (f"Nota {n1} + {n2} = {m}")
 # n=nota ; m=média das notas
 
 
